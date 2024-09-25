4a78476eb84b3f407ffcdc43dbae7eefda326876
basov-roman <basov.roman.work@gmail.com>
2024-09-25 11:25:46 +0300

    Added correct display of the avatar and a link to the live version of the project in the documentation.

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ This is a project for customizing a user profile.
 1. Run the command "yarn install"
 
 2. Run the command "yarn dev"
+
+## A link to the live version of the project
+
+https://marmend-profile-settings.vercel.app/
--- src/components/ProfileSettingsPhoto/ProfileSettingsPhoto.tsx
+++ src/components/ProfileSettingsPhoto/ProfileSettingsPhoto.tsx
@@ -1,11 +1,9 @@
+import mockAvatar from "/src/assets/mock/avatar.png";
+
 const ProfileSettingsPhoto = () => {
   return (
     <div className="flex gap-4">
-      <img
-        className="w-[84px] h-auto"
-        src="/src/assets/mock/avatar.png"
-        alt="Profile avatar"
-      />
+      <img className="w-[84px] h-auto" src={mockAvatar} alt="Profile avatar" />
 
       <div className="flex flex-col gap-2">
         <button className="border-main-primary bg-transparent px-2 m:px-[45.5px] py-2 border rounded-lg font-medium text-main-primary text-sm leading-[22px]">
